d946b375d339a67ff0c5d5084f600156ea942ceb
Abbas Own <abbas_own@ismonnet.edu.it>
2025-12-10 09:29:19 +0100

    sistemazione database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", );
-define("DB_NAME", "portale_studenti");
+define("DB_NAME", "Registro-Elettronico");
 
 define("DB_CHARSET", "utf8mb4");
 
