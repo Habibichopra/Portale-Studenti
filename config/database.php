ee52a69e3c8225ef46c5d46d6497e849fcbcb25a
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-07 08:54:29 +0100

    Correzione errore in config/databse.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", );
+define("DB_PASS", "");
 define("DB_NAME", "Registro-Elettronico");
 
 define("DB_CHARSET", "utf8mb4");
