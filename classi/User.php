77025a0e0324f1ef6df99a2404460ec4126f024a
herrera05 <matteoroberto.2005@gmail.com>
2026-01-07 09:53:30 +0100

    Dichiarazione variabili e costruttore

--- classi/User.php
+++ classi/User.php
@@ -2,7 +2,15 @@
 require_once __DIR__ . '/Database.php';
 
 class User {
-    
+    private $conn;
+    private $nome_tabella = "users";
+
+    public function __construct() {
+        $database = Database::getInstance();
+        $this->conn = $database->getConnection();
+
+    }
+
 }
 
 
