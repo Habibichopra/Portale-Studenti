1eb77031ac669f557a08d4eb497e3331b18756de
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-12 09:37:28 +0100

    implementazione footer

---
+++ inclusi/footer.php
@@ -0,0 +1,17 @@
+<div>
+    <footer class="footer">
+        <div class="container">
+            <div>
+                <h3>Portale Studenti</h3>
+                <p>La piattaforma completa per la gestione della tua carriera scolastica universitaria</p>
+            </div>
+            <div>
+                <p>&copy; <?php echo date("Y"); ?> Portale Studenti. All rights reserved.</p>
+            </div>
+
+            <script src="<?php echo BASE_URL; ?>assets/js/main.js"></script>
+            <script src="<?php echo BASE_URL; ?>assets/js/validazione.js"></script>
+
+        </div>
+    </footer>
+</div>
