cdd6735465e8aaab884cfba8171f9594d8c0df0f
herrera05 <matteoroberto.2005@gmail.com>
2026-01-15 11:22:41 +0100

    implementazione costruttore classe compito

--- classi/Compito.php
+++ classi/Compito.php
@@ -6,5 +6,10 @@ class Compito {
     private $nome_tabella = "compiti";
     private $tabella_corsi = "corsi";
     private $tabella_iscrizioni = "iscrizioni";
+
+    public function __construct() {
+        $database = Database::getInstance();
+        $this->conn = $database->getConnection();
+    }
 }
 ?>
