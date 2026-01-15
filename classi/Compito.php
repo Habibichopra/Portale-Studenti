969af63818d684301592873a190351cdcb59019c
herrera05 <matteoroberto.2005@gmail.com>
2026-01-15 11:26:06 +0100

    dichiarazione metodi per la classe compito

--- classi/Compito.php
+++ classi/Compito.php
@@ -11,5 +11,40 @@ class Compito {
         $database = Database::getInstance();
         $this->conn = $database->getConnection();
     }
+    
+    //creazione di un nuovo compito
+    public function creaCompito($corso_id, $titolo, $descrizione, $scadenza, $punti_max, $allegato = null) {
+    
+    }
+
+    //aggorna compito
+    public function aggiornaCompito($id, $dati) {
+    
+    }
+
+    //elimina un compito
+    public function eliminaCompito($id) {
+    
+    }
+
+    //compito in base al id
+    public function getCompitoById($id) {
+    
+    }
+
+    //compiti in base al corso
+    public function getCompitiByCorso($corso_id) {
+
+    }
+
+    //trovare compiti scaduti
+    public function getCompitiScaduti($corso_id = null) {
+
+    }
+
+    //compiti in scadenza nei prossimi giorni
+    public function getCompitiProssimi($giorni = 7, $studente_id = null) {
+    
+    }
 }
 ?>
