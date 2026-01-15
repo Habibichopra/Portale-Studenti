e1fd276ce88db9cbfa5fe05b9d6f1b01cf9db515
herrera05 <matteoroberto.2005@gmail.com>
2026-01-15 11:21:31 +0100

    dichiarazione metodi per la classe compito

--- classi/Compito.php
+++ classi/Compito.php
@@ -2,6 +2,9 @@
 require_once __DIR__ . '/Database.php';
 
 class Compito {
-
+    private $conn;
+    private $nome_tabella = "compiti";
+    private $tabella_corsi = "corsi";
+    private $tabella_iscrizioni = "iscrizioni";
 }
 ?>
