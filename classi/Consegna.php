742d8989e584ea071faf8d49d123c66d9c9335fd
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 09:44:03 +0100

    metodo per consegnare un compito

--- classi/Consegna.php
+++ classi/Consegna.php
@@ -15,7 +15,40 @@ class Consegna {
 
     //cosnega del compito da parte del stusente
     public function consegnaCompito($compito_id, $studente_id, $file, $note) {
+        $cartellaDestinazione = __DIR__ . "/../importazioni/consegne/";
+        
+        if (!file_exists($cartellaDestinazione)) {
+            mkdir($cartellaDestinazione, 0755, true);
+        }
 
+        $estensioneFile = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
+        $nomeNuovoFile = $compito_id . "_" . $studente_id . "_" . time() . "." . $estensioneFile;
+        $percorsoFileServer = $cartellaDestinazione . $nomeNuovoFile;
+        $percorsoFileDatabase = "importazioni/consegne/" . $nomeNuovoFile;
+
+        if (move_uploaded_file($file["tmp_name"], $percorsoFileServer)) {
+            
+            $query = "INSERT INTO " . $this->nome_tabella . " 
+                      (compito_id, studente_id, file_consegna, note_studente, stato) 
+                      VALUES (:compito_id, :studente_id, :file, :note, 'consegnato')";
+            
+            $stmt = $this->conn->prepare($query);
+
+            $note = htmlspecialchars(strip_tags($note));
+
+            $stmt->bindParam(":compito_id", $compito_id);
+            $stmt->bindParam(":studente_id", $studente_id);
+            $stmt->bindParam(":file", $percorsoFileDatabase);
+            $stmt->bindParam(":note", $note);
+
+            if ($stmt->execute()) {
+
+                $ultimoID = $this->conn->lastInsertId();
+                $this->checkRitardo($ultimoID); 
+                return true;
+            }
+        }
+        return false;
     }
 
     //valutazione del compito da parte del professore
