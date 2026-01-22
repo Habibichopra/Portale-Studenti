98c9fadb1aee6cb5940eb34a65608c16d294fc69
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 12:24:51 +0100

    istanze classe comunicazione

--- classi/Comunicazione.php
+++ classi/Comunicazione.php
@@ -2,6 +2,11 @@
 require_once __DIR__ . '/Database.php';
 
 class Comunicazione {
-
+    private $conn;
+    private $nome_tabella = "comunicazioni";
+    private $tabella_users = "users";
+    private $tabella_corsi = "corsi";
 }
+
+
 ?>
