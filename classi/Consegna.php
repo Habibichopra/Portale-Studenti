1505291dbe15ad58c4a9eefa7e4e02c90a0e043d
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 09:25:09 +0100

    istanze classe consegna

--- classi/Consegna.php
+++ classi/Consegna.php
@@ -2,6 +2,11 @@
 require_once __DIR__ . '/Database.php';
 
 class Consegna {
+    private $conn;
+    private $nome_tabella = "consegne";
+    private $tabella_compiti = "compiti";
+    private $tabella_users = "users";
+    private $tabella_corsi = "corsi";
 
 }
 
