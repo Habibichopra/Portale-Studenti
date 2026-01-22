3410327f3666c736f852c8b308afc331f0a4a114
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 12:15:06 +0100

    costruttore classe materiale

--- classi/Materiale.php
+++ classi/Materiale.php
@@ -5,6 +5,12 @@ require_once __DIR__ . '/Database.php';
 class Materiale {
     private $conn;
     private $nome_tabella = "materiali";
+
+
+    public function __construct() {
+        $database = Database::getInstance();
+        $this->conn = $database->getConnection();
+    }
 }
 
 ?>
