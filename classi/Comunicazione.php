3e9eb3a3b528c06801eb60f847ed5ab450e1a5f2
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 12:25:14 +0100

    costruttore

--- classi/Comunicazione.php
+++ classi/Comunicazione.php
@@ -6,7 +6,12 @@ class Comunicazione {
     private $nome_tabella = "comunicazioni";
     private $tabella_users = "users";
     private $tabella_corsi = "corsi";
-}
 
+    public function __construct() {
+        $database = Database::getInstance();
+        $this->conn = $database->getConnection();
+    }
+
+}
 
 ?>
