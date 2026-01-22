227d7fe8aac427560bad6760ed67f300b538ce49
herrera05 <matteoroberto.2005@gmail.com>
2026-01-22 09:14:25 +0100

    dichiarazione metodi utili

--- classi/Corso.php
+++ classi/Corso.php
@@ -12,8 +12,50 @@ class Corso {
         $this->conn = $database->getConnection();
     }
 
+    //creazione di un corso
+    public function createCorso($nome, $codice, $descrizione, $anno, $professore_id, $crediti) {
 
+    }
 
+    //aggiorna corso
+    public function aggiornaCorso($id, $dati) {
+    
+    }
+
+    //eliminazione corso
+    public function eliminaCorso($id) {
+
+    }
+
+    //ottieni corso con id
+    public function getCorsoById($id) {
+    
+    }
+
+    //get di tutti i corsi
+    public function getAllCorsi() {
+    
+    }   
+
+    //get corsi in base al professorei
+    public function getCorsiByProfessore($professore_id) {
+
+    }
+
+    //get corsi in base al studente
+    public function getCorsiByStudente($studente_id) {
+
+    }
+
+    //iscrizione studente al corso
+    public function iscriviStudente($studente_id, $corso_id) {
+    
+    }
+
+    //rimuovi iscrizione
+    public function rimuoviIscrizione($iscrizione_id) {
+
+    }
 
 
 
