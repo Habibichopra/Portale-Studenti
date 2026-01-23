311583e3fbce329e593ebf97b4558c4d8cbec672
herrera05 <matteoroberto.2005@gmail.com>
2026-01-23 10:06:33 +0100

    metodo er inviare una comunicazione

--- classi/Comunicazione.php
+++ classi/Comunicazione.php
@@ -14,7 +14,34 @@ class Comunicazione {
 
 
     public function inviaComunicazione($mittente_id, $destinatario_id, $corso_id, $oggetto, $messaggio) {
-    
+        $query = "INSERT INTO " . $this->nome_tabella . " 
+                  (mittente_id, destinatario_id, corso_id, oggetto, messaggio, data_invio, letto) 
+                  VALUES (:mittente, :destinatario, :corso, :oggetto, :messaggio, NOW(), 0)";
+        
+        $stmt = $this->conn->prepare($query);
+
+        $oggetto = htmlspecialchars(strip_tags($oggetto));
+        $messaggio = htmlspecialchars(strip_tags($messaggio));
+
+        $stmt->bindParam(":mittente", $mittente_id);
+        
+        if (empty($destinatario_id)) {
+            $destinatario_id = null;
+        }
+        $stmt->bindParam(":destinatario", $destinatario_id);
+
+        if (empty($corso_id)) {
+            $corso_id = null;
+        }
+        $stmt->bindParam(":corso", $corso_id);
+
+        $stmt->bindParam(":oggetto", $oggetto);
+        $stmt->bindParam(":messaggio", $messaggio);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     //recuparare tutte le comunicazioni per un utente specifico
