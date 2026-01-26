c11670741ce0cfe96ef1e9e38609aef8fa3a03c3
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 10:24:01 +0100

    istanza oggetti e recupero dei dati

--- studente/dashboard.php
+++ studente/dashboard.php
@@ -7,4 +7,10 @@ require_once '../inclusi/session_check.php';
 require_once '../classes/Compito.php';
 require_once '../classes/Voto.php';
 require_once '../classes/Corso.php';
+
+$studente_id = $_SESSION['user_id'];
+
+$compitoObj = new Compito();
+$votoObj = new Voto();
+$corsoObj = new Corso();
 ?>
