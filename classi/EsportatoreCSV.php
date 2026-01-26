4d3cf23f131336c7611f6b893550cdc00185a37e
herrera05 <matteoroberto.2005@gmail.com>
2026-01-26 12:33:12 +0100

    costruttore

--- classi/EsportatoreCSV.php
+++ classi/EsportatoreCSV.php
@@ -4,6 +4,11 @@ require_once __DIR__ . '/Database.php';
 class EsportatoreCSV  {
     private $conn;
     private $export_dir;
+
+    public function __construct() {
+        $database = Database::getInstance();
+        $this->conn = $database->getConnection();
+    }
 }
 
 ?>
