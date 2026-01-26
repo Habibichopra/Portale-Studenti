53dfbd01fba3ce49811619afeb1ec0c8a637580c
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 12:44:33 +0100

    se il form di export CSV è stato inviato

--- studente/voti.php
+++ studente/voti.php
@@ -10,4 +10,11 @@ require_once '../classi/EsportatoreCSV.php';
 $studente_id = $_SESSION['user_id'];
 $votoObj = new Voto();
 $csvExporter = new EsportatoreCSV();
+
+
+$download_link = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['export_csv'])) {
+    
+}
+
 ?>
