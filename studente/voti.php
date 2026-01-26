450ec887a44349b1f3bff450de4578e08309a27a
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 12:27:54 +0100

    importo classi necessarie

--- studente/voti.php
+++ studente/voti.php
@@ -3,4 +3,9 @@ require_once '../config/config.php';
 
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
+
+require_once '../classi/Voto.php';
+require_once '../classi/EsportatoreCSV.php';
+
+
 ?>
