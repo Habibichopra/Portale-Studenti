bfeb064f18142f2b25e84f1f13ab1182fcbccdbb
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 10:13:49 +0100

    scruttura barra di navigazione

--- inclusi/nav.php
+++ inclusi/nav.php
@@ -1,3 +1,11 @@
 <?php
 require_once __DIR__ . '/../config/config.php'; 
 ?>
+
+<nav class="navbar">
+
+        <a href="<?php echo BASE_URL; ?>index.php" class="logo">
+            <i class="fas fa-graduation-cap"></i> Portale-Studenti
+        </a>
+
+</nav>
