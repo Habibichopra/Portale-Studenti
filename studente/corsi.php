f047406c400461463c9dfffba7a620e0cb7ec831
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 17:13:36 +0100

    correzione errori

--- studente/corsi.php
+++ studente/corsi.php
@@ -4,7 +4,7 @@ require_once '../config/config.php';
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
 
-require_once '../classsi/Corso.php';
+require_once '../classi/Corso.php';
 
 $corsoObj = new Corso();
 $studente_id = $_SESSION['user_id'];
