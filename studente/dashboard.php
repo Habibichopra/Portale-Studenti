1640b72ef2f31c01f9e1de31b5dc7e66a18e5165
Own Abbas <94720597+Habibichopra@users.noreply.github.com>
2026-01-26 10:23:25 +0100

    incluszione delle classi

--- studente/dashboard.php
+++ studente/dashboard.php
@@ -3,4 +3,8 @@ require_once '../config/config.php';
 
 $required_ruolo = 'studente';
 require_once '../inclusi/session_check.php';
+
+require_once '../classes/Compito.php';
+require_once '../classes/Voto.php';
+require_once '../classes/Corso.php';
 ?>
