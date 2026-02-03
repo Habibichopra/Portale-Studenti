5c080cbc2605f80d3c67213c0356884115e61e81
herrera05 <matteoroberto.2005@gmail.com>
2026-02-03 23:12:27 +0100

    modifiche

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "Portale-Studenti");
+define("DB_NAME", "PortStud");
 
 define("DB_CHARSET", "utf8mb4");
 
